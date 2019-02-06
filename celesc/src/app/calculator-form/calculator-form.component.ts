7f0289d52cfb6d86677efaa00923fa452e429ea4
Gustavo Olegário <gustavo-olegario@hotmail.com>
2019-02-05 22:50:46 -0200

    Message of success and error

--- celesc/src/app/calculator-form/calculator-form.component.ts
+++ celesc/src/app/calculator-form/calculator-form.component.ts
@@ -10,20 +10,22 @@ export class CalculatorFormComponent implements OnInit {
   consuption = null;
   result = 0.0;
   clicked = false;
+  error = false;
 
   constructor() { }
 
   ngOnInit() { }
 
   calculate() {
-    if (this.consuption === null) {
-      console.log('vsfff');
+    if (this.consuption === null || this.consuption === undefined) {
+      this.error = true;
     } else {
       if (this.consuption <= 150) {
         this.result = this.consuption * 0.654;
       } else {
         this.result = 98.1 + ((this.consuption - 150) * 0.782);
       }
+      this.error = false;
       this.clicked = true;
     }
   }
